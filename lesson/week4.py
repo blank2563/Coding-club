9188018385151f999f870cd6d6790af095ae07f1
Blank_2563 <alternateacc0310@gmail.com>
2023-11-08 15:06:09 -0500

    week4

---
+++ lesson/week4.py
@@ -0,0 +1,15 @@
+def Hayson():
+    print('Hayson? Hayson!')
+
+Hayson()
+
+def OddOrEven(n):
+    if n % 2 == 0:
+        return 'Even'
+    else:
+        return 'Odd'
+
+OddOrEven(5)
+a = OddOrEven(5)
+print(OddOrEven(16))
+print(a)
